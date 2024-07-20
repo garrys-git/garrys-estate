26223586ee1ab068fc3c22d2a5ee40f0629d9c87
gsspg <gauravlandge12307@gmail.com>
2024-07-20 12:31:14 +0530

    final changes for show listings

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -4,8 +4,7 @@ import {
   deleteListing,
   updateListing,
   getListing,
-  getListings
-
+  getListings,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -13,9 +12,8 @@ const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
-router.post('/update/:id', verifyToken, updateListing);
-router.get('/get/:id', getListing);
-router.get('/get', getListings);
-
+router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 export default router;
